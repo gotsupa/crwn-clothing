5604037e64c63dab48ab212f49be7edf7a2a0217
Supachai <supachai.chanu@gmail.com>
2021-07-16 19:26:42 +0700

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -62,7 +62,7 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn='foo'>
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn='foo'>
               Sign in with Google
             </CustomButton>
           </div>
